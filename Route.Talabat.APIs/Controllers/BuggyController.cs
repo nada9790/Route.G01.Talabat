2292d412cea25994619ebf73c441ab947df02f7b
Nada Mohamed <nada971990@gmail.com>
2024-04-23 14:43:23 +0200

    Error Handling

---
+++ Route.Talabat.APIs/Controllers/BuggyController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Talabat.Repository.Data;
+
+namespace Route.Talabat.APIs.Controllers
+{
+
+    public class BuggyController : APIBaseController
+    {
+        private readonly StoreContext _dbContext;
+
+        public BuggyController(StoreContext dbcontext)
+        {
+            this._dbContext = dbcontext;
+        }
+
+        [HttpGet("NotFound")]
+        public ActionResult GetNotFoundRequest()
+        {
+            var Product = _dbContext.Products.Find(100);
+            if (Product is null) return NotFound();
+            return Ok(Product);
+        }
+
+        [HttpGet("ServerError")]
+        public ActionResult GetServerError()
+        {
+            var Product = _dbContext.Products.Find(100);
+            var ProductToReturn = Product.ToString();
+            return Ok(ProductToReturn);
+        }
+
+        [HttpGet("BadRequest")]
+        public ActionResult GetBadRequest()
+        {
+            return (BadRequest());
+        }
+
+
+        [HttpGet("BadRequest/{id}")]
+        public ActionResult GetbadRequest(int id)
+        {
+           
+            return Ok();
+        }
+    }
+}
